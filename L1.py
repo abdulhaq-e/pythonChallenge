c623a24af7c0ad61a7ab178c63a2f3d5462f0a86
Abdulhaq E <el.ingeniero09@gmail.com>
2013-05-29 05:50:31 +0100

    finished level 1

--- L1.py
+++ L1.py
@@ -1,16 +1,35 @@
 def sol1(f):
     
-    text = open(f).read()
     import string
+
+    text = open(f).read()
     shufd = [string.ascii_lowercase[x+2] for x in xrange(24)]
     shufd.append('a')
     shufd.append('b')
 
     nonshufd = [string.ascii_lowercase[x] for x in xrange(26)]
     
-    #for i in range(26):
-    newText = [string.replace(text,nonshufd[i],shufd[i]) for i in range(26)]
-    print shufd[0], nonshufd[0]
-    #print newText
+    newText = text
+    for i in range(26):
+        newText = string.replace(newText,nonshufd[i],shufd[i])
+        print newText
+    print shufd, nonshufd
+    print newText
+
+
+def sol2 (f):
+    """"""
+
+    import string
+    text = open(f).read()
     
-    
+    shufd = [string.ascii_lowercase[x+2] for x in xrange(24)]
+    shufd.append('a')
+    shufd.append('b')
+    shufd = string.join(shufd, '')
+    nonshufd = [string.ascii_lowercase[x] for x in xrange(26)]
+    nonshufd = string.join(nonshufd, '')
+
+    trans_table = string.maketrans(nonshufd,shufd)
+    newText = string.translate(text,trans_table)
+    print newText
